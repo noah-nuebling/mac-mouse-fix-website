31d8a04ee82d45be1d9799fa24a0cdea3a519fd0
Noah Nuebling <noah.n.public@gmail.com>
2023-09-05 16:34:37 +0200

    Added unused NormalFeatureCard.vue and commented out 122% zoom on body

---
+++ components/NormalFeatureCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <FeatureCard ref="moveSpacesCard" class="feature-card col-span-auto bg-gradient-pink shadow-md shadow-red-900/30 cursor-pointer group" @click="$refs.moveSpacesCard.expand()">
+    <template v-slot:top> 
+      <h3 class="dark-bg m-6">{{ title }}</h3>
+    </template>
+    <template v-slot:default>
+      <div class="flex flex-row items-center h-full m-6 mt-0 mb-3">
+        <div>
+          <p class="dark-bg">Hold a button on your mouse, then drag the mouse left or right to move to another Space with a smooth and intuitive animation which follows the movements of your hand precisely.</p>
+          <p class="dark-bg">It works just like swiping left of right with 4 fingers on an Apple Trackpad.</p>
+        </div>
+        <!-- <div class="ml-4">
+          <img src="~/assets/img/play-dark.png" alt="trackpad" class="w-20">
+        </div> -->
+        
+      </div>
+      <div class="rounded-[0px] border-[4px] m-[-4px] border-gray-50/20 min-h-[52px] flex items-center justify-center mt-5 cursor-pointer group-hover:underline">
+        <a class="text-base dark-bg text-gray-50/90 text-center"><span class="">See It in Action</span><span class="inline-space-[8]"/><img src="~/assets/img/play.circle@2x.png" alt="Play Video Icon" class="ml-[0px] inline h-[1.16rem] align-[-3.6px] contrast-200"></a>
+      </div>
+    </template>
+    <template v-slot:expanded>
+      <div class="video-wrapper">
+        <video id="vid1" preload="auto" playsinline loop autoplay alt="Remap Demo" class="object-cover w-full h-full">
+          <source src="~assets/video/remap_demo_old.mp4#t=0.1" type="video/mp4">
+        </video>
+      </div>
+    </template>
+    <!-- <template v-slot:bottom>
+    </template> -->
+  </FeatureCard>
+</template>
+
+<script setup lang="ts">
+
+defineProps({
+  contentPath: String,
+  videoPath: String,
+})
+
+</script>
+
+<style scoped lang="postcss">
+
+</style>
