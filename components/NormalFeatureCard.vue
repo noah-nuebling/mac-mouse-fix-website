9d031eefe26fd6b1ffbdedaa246690a8bbbc45fe
Noah Nuebling <noah.n.public@gmail.com>
2023-10-01 17:28:09 +0200

    Made card text larger. Still using tailwind text sizes. Might want to fine-tune at some point. But it's better now I think. More scannable. Tried to give play button a shadow but doesn't work. It's okay.

--- components/NormalFeatureCard.vue
+++ components/NormalFeatureCard.vue
@@ -5,16 +5,17 @@
 <template>
   <FeatureCard ref="moveSpacesCard" class="feature-card col-span-auto cursor-pointer group shadow-md" @click="$refs.moveSpacesCard.expand()">
     <template v-slot:top> 
-      <h3 class="text-lg font-bold text-white/100 m-6 shadow-black/100">{{ $t(titleKey) }}</h3>
+      <h3 class="text-2xl font-bold text-white/100 m-6 shadow-black/100">{{ $t(titleKey) }}</h3>
     </template>
     <template v-slot:default>
       <div class="flex flex-row items-stretch h-full m-6 mt-0 mb-3">
         <div>
-          <p class="text-white/100 whitespace-pre-wrap shadow-black/100">{{ $t(bodyKey) }}</p>
+          <p class="text-base text-white/100 whitespace-pre-wrap shadow-black/100">{{ $t(bodyKey) }}</p>
         </div>        
       </div>
       <div class="rounded-[0px] border-[4px] m-[-4px] border-gray-50/20 min-h-[52px] flex items-center justify-center mt-5 cursor-pointer group-hover:underline">
-        <a class="text-base dark-bg text-white/100 text-center shadow-black/100"><span class="">{{ $t('feature-card.expand-button') }}</span><span class="inline-space-[8]"/><img src="~/assets/img/play.circle@2x.png" alt="Play Video Icon" class="ml-[0px] inline h-[1.16rem] align-[-3.6px] contrast-200"></a>
+        <!-- vvv Can't seem to give the inline play button image a shadow vvv -->
+        <a class="text-lg dark-bg text-white/100 text-center shadow-black/100"><span class="">{{ $t('feature-card.expand-button') }}</span><span class="inline-space-[8]"/><img src="~/assets/img/play.circle@2x.png" alt="Play Video Icon" class="ml-[0px] inline h-[1.16rem] align-[-3.6px] filter brightness-0 invert drop-shadow-xl"></a>
       </div>
     </template>
     <template v-slot:expanded>
