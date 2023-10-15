a0058a72ece7e44782618a8db01a0f2ac469f4ce
Noah Nuebling <noah.n.public@gmail.com>
2023-10-15 15:25:37 +0200

    Progress on prototyping action table section

--- components/WhiteFeatureCard.vue
+++ components/WhiteFeatureCard.vue
@@ -6,7 +6,7 @@
   <FeatureCard 
     ref="thisCard"
     class="feature-card col-span-auto group shadow-md rounded-[24px] max-w-[1000px]" 
-    borderClass="border-[5px] border-gray-50/25 rounded-[24px]"
+    borderClass="border-[1px] border-gray-200/100 rounded-[24px]"
 
     @click="$refs.thisCard.expand()">
 
@@ -16,12 +16,12 @@
     <template v-slot:default>
       <div class="flex flex-col items-center h-full m-6 mt-0 mb-3">
         <div class="min-w-full -mx-6">
-          <img src="~assets/img/mac-menubar-big-sur.png" alt="">
+          <!-- <img src="~assets/img/mac-menubar-ventura.png" alt=""> -->
         </div>
-        <div class="max-w-[700px] -mt-5 -mb-14">
-          <img src="~assets/img/action-table-shadow.png" class="" alt="">
+        <div class="w-[1700px] mt-1 mb-[-240px] translate-x-[22%]">
+          <img src="~assets/img/mmf-on-studio-display-3.png" class="" alt="">
         </div>
-        <div class="bg-white/50 rounded-[15px] backdrop-blur-xl shadow-md px-5 py-3">
+        <div class="bg-white/50 rounded-[15px] backdrop-blur-xl shadow-md px-5 py-3 -mt-5 mb-5">
           <!-- <p class="text-base text-slate-900 whitespace-pre-wrap text-shadow-none shadow-black/100 text-start">{{ $t(bodyKey) }}</p> -->
           
           <p class="text-base text-slate-900/90 whitespace-pre-wrap text-shadow-none shadow-black/100 text-start mb-1">
@@ -37,9 +37,9 @@
           </p>
         </div>
       </div>
-      <div class="rounded-[0px] border-t-[4px] mt-5 border-gray-50/25 min-h-[52px] flex items-center justify-center cursor-pointer group-hover:underline">
+      <div class="bg-white/50 backdrop-blur-xl rounded-[0px] border-t-[1px] mt-5 border-gray-200/100 min-h-[52px] flex items-center justify-center cursor-pointer group-hover:underline">
         <!-- vvv Can't seem to give the inline play button image a shadow vvv -->
-        <a class="text-lg dark-bg text-white text-center shadow-black/100"><span class="">{{ $t('feature-card.expand-button') }}</span><span class="inline-space-[8]"/><img src="~/assets/img/play.circle@2x.png" alt="Play Video Icon" class="ml-[0px] inline h-[1.16rem] align-[-3.6px] filter brightness-0 invert drop-shadow-xl"></a>
+        <a class="text-lg dark-bg text-slate-900/90 text-center shadow-black/100"><span class="">{{ $t('feature-card.expand-button') }}</span><span class="inline-space-[8]"/><img src="~/assets/img/play.circle-dark@2x.png" alt="Play Video Icon" class="ml-[0px] inline h-[1.16rem] align-[-3.6px] filter brightness-0 drop-shadow-xl"></a>
       </div>
     </template>
     <template v-slot:expanded>
