6d93ec91d30a24b07422b90105896de711c0a1e3
Noah Nuebling <noah.n.public@gmail.com>
2023-10-18 10:40:50 +0200

    Shortened trackpad-features.body text

--- locales/en-US.js
+++ locales/en-US.js
@@ -5,7 +5,7 @@ export default {
 
   'trackpad-features.title': "Macs Are Best\nWith a {track}\n...Right?",
   'trackpad-features.title.track': "Trackpad",
-  'trackpad-features.body': 'Mac Mouse Fix brings all the functions of an official Apple Trackpad to your third-party mouse.* And all the interactions feel just as effortless, responsive, smooth, and polished as they do on the Trackpad.\n\nAdditionally, you receive the benefits of a traditional mouse, such as lower cost, enhanced comfort, and improved accuracy.',
+  'trackpad-features.body': 'Do everything you do on an Apple Trackpad on your third-party mouse.* All interactions feel just as smooth and intuitive as they do on the Trackpad.',
   'trackpad-features.cards.hint.0': 'Explore the range of Trackpad functionalities which Mac Mouse Fix brings to your mouse in the cards below.*',
   'trackpad-features.cards.hint.1': 'Click any card for a video demo.',
   
