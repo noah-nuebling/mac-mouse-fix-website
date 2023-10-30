680e126319b5e3cc15e1db6f0cd473119c04a410
Noah Nuebling <noah.n.public@gmail.com>
2023-10-30 13:02:05 +0100

    Added Affinity Photo project where we figured out the intro design

--- components/Intro.vue
+++ components/Intro.vue
@@ -10,7 +10,7 @@
     <div class="w-[100%] h-[calc(100vh-6rem)]">
       <div class=" h-[100%] w-[100%] flex flex-col items-center justify-center translate-y-[-5rem] scale-[1.3]"> 
         <img :src="mmfIconImagePath" alt="Mac Mouse Fix Icon" class="h-[14rem]">
-        <h1 class="font-[700] text-[5.0rem] text-black/90 mt-[-1.75rem] mb-[-1.25rem]">Mac Mouse Fix</h1>
+        <h1 class="font-[700] text-[5.0rem] text-black/90 mt-[1.75rem] mb-[-1.25rem]">Mac Mouse Fix</h1>
         <p class="text-black opacity-[0.7] mb-[1.5rem]">Make Your $10 Mouse Better Than an Apple Trackpad</p>
         <div class="bg-blue-500 rounded-[5rem]">
           <p class="text-white mx-[0.75em] my-[0.25em]">Download</p>
