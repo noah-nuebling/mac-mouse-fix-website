e690ef198d1f24eb7360e2de6cf926fd6bcd48b5
Noah Nuebling <noah.n.public@gmail.com>
2023-10-30 22:37:36 +0100

    Load / intro animation is pretty good now

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">
+  <div class="mt-[0rem]">
 
     <!-- Background -->
 
@@ -7,9 +7,9 @@
 
     <!-- Content -->
 
-    <div class="group w-[100%] h-[calc(100vh-6rem)] duration-[0.8s] ease-[cubic-bezier(0.2,0,0.2,1)]">
-      <div ref="innerContent" class="h-[100%] w-[100%] flex flex-col items-center justify-center translate-y-[-5rem] transition-[transform] duration-[inherit] ease-[inherit]"> 
-        <img ref="mmfIcon" :src="mmfIconImagePath" alt="Mac Mouse Fix Icon" class="h-[14rem] transition-[opacity] opacity-0 duration-[inherit] ease-[inherit]">
+    <div class="group w-[100%] h-[calc(100vh-5rem)] duration-[0.8s] ease-[cubic-bezier(0.2,0,0.2,1)]">
+      <div ref="innerContent" class="h-[100%] w-[100%] flex flex-col items-center justify-center translate-y-[-6rem] transition-[transform] duration-[inherit] ease-[inherit] border-[0px]"> 
+        <img ref="mmfIcon" :src="mmfIconImagePath" alt="Mac Mouse Fix Icon" class="h-[14rem] transition-[opacity] opacity-0 duration-[inherit] ease-[inherit] border-[0px] mt-[-2rem]">
         <h1 ref="mmfName" class="font-[700] text-[5.0rem] text-black/90 mt-[1.75rem] mb-[-1.25rem] transition-transform duration-[inherit] ease-[inherit] animate-pulse scale-[0.8]">Mac Mouse Fix</h1>
         <p ref="tagline" class="text-black mb-[1.5rem] transition-[opacity] opacity-0 duration-[inherit] ease-[inherit]">Make Your $10 Mouse Better Than an Apple Trackpad</p>
         <div ref="downloadButton" class="bg-blue-500 rounded-[5rem] ransition-[opacity] opacity-0 duration-[inherit] ease-[inherit]">
@@ -44,8 +44,8 @@ const downloadButton: Ref<HTMLElement|null> = ref(null)
 onMounted(() => {
 
   /* Intro animation */
-  innerContent.value!.classList.remove('translate-y-[-5rem]')
-  innerContent.value!.classList.add('translate-y-[-8rem]')
+  innerContent.value!.classList.remove('translate-y-[-6rem]')
+  innerContent.value!.classList.add('translate-y-[-0rem]')
   mmfIcon.value!.classList.remove('opacity-0')
   mmfName.value!.classList.remove('animate-pulse', 'scale-[0.8]')
   tagline.value!.classList.remove('opacity-0')
