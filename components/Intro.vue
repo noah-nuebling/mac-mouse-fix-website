1f21ef47411cddce7cefb5795a5b91595eb26bd8
Noah Nuebling <noah.n.public@gmail.com>
2023-11-03 18:53:28 +0100

    Progress on intro animation

--- components/Intro.vue
+++ components/Intro.vue
@@ -4,23 +4,23 @@
         Notes: 
         - Setting z negative prevents scrolling in the qutoes. Not sure why. -->
 
-  <div ref="outerContainer" class="w-[100%] h-[calc(100vh-5rem)] relative mt-[-0rem] z-10">
+  <div ref="outerContainer" class=" relative mt-[-0rem] z-10">
 
     <!-- BG + Color Splashes -->
 
-    <div ref="backgroundContainer" class="bg-transparent w-[100vw] h-[100vh] overflow-x-visible overflow-y-visible absolute left-[50%] translate-x-[-50%] top-0 bottom-0 -z-10">
-      <img ref="colorSplash1" :src="colorSplashImagePath" alt="Color Splash" :class="['.color-splash-pulse1 absolute min-w-[80rem] top-0 left-0 translate-x-[calc(-50%-(-15%))] translate-y-[calc(-50%-12%)] scale-[1.1] z-10 opacity-0']">
-      <img ref="colorSplash2" :src="colorSplashImagePath" alt="Color Splash" :class="['.color-splash-pulse2 absolute min-w-[80rem] bottom-0 right-0 translate-x-[calc(50%+(-15%))] translate-y-[calc(50%+12%)] scale-[1.1] z-10 opacity-0']">
+    <div ref="backgroundContainer" class="bg-transparent w-[100vw] h-[100vh] overflow-x-visible overflow-y-visible absolute left-[50%] translate-x-[-50%] top-0 bottom-0 z-0">
+      <img ref="colorSplash1" :src="colorSplashImagePath" alt="Color Splash" :class="['.color-splash-pulse1 absolute min-w-[80rem] top-0 left-0 translate-x-[calc(-50%-(-15%))] translate-y-[calc(-50%-12%)] scale-[1.1] -z-10 opacity-0']">
+      <img ref="colorSplash2" :src="colorSplashImagePath" alt="Color Splash" :class="['.color-splash-pulse2 absolute min-w-[80rem] bottom-0 right-0 translate-x-[calc(50%+(-15%))] translate-y-[calc(50%+12%)] scale-[1.1] -z-10 opacity-0']">
     </div>
 
     <!-- Initial Content -->
 
-    <div class="flex items-center justify-center group w-[100%] h-[calc(100vh-5rem)] duration-[0.8s] ease-[cubic-bezier(0.4,0,0.2,1)]">
+    <div class="flex items-center justify-center group w-[100%] h-[calc(100vh-0rem)] duration-[0.8s] ease-[cubic-bezier(0.4,0,0.2,1)]">
       <div class="h-fit w-fit border-[0px] relative">
         <div ref="innerContent" :class="['h-[100%] w-[100%] relative flex flex-col items-center justify-center border-[0px] translate-y-[-6rem] -z-20']"> 
           <img ref="mmfIcon" :src="mmfIconImagePath" alt="Mac Mouse Fix Icon" :class="['h-[14rem] border-[0px] mt-[-2rem] opacity-0']">
           <h1 ref="mmfName" :class="['font-[700] text-[5.0rem] text-black/90 mt-[1.75rem] mb-[-1.25rem] scale-[0.8]', playLoadingAnimation ? 'animate-pulse' : '']">Mac Mouse Fix</h1>
-          <p ref="tagline" :class="['text-black mb-[1.5rem] opacity-0']">Make Your $10 Mouse Better Than an Apple Trackpad</p>
+          <p ref="introTagline" :class="['text-black mb-[1.5rem] opacity-0']">Make Your $10 Mouse Better Than an Apple Trackpad</p>
           <div ref="downloadButton" :class="['bg-blue-500 rounded-[5rem] opacity-0']">
             <p class="text-white mx-[0.75em] my-[0.25em] text-[1.0rem]">Download</p>
           </div>
@@ -31,12 +31,13 @@
     <!-- Tagline -->
 
     <div ref="taglineContainer" class="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center opacity-0">
-      <p class="text-white font-[500] text-[2rem]" >Make Your $10 Mouse Better Than an Apple Trackpad</p>
+      <p ref="tagline" class="text-white font-[500] text-[2rem]" >Make Your $10 Mouse Better Than an Apple Trackpad</p>
     </div>
 
     <!-- Quote cards -->
 
-    <div ref="quoteContainer" class="hidden absolute top-0 left-0 right-0 bottom-0 w-full h-full overflow-auto z-30">
+    <div ref="quoteContainer" class="invisible absolute top-0 left-0 right-0 bottom-0 w-full h-full overflow-hidden z-30 border-green-500">
+      <div class="h-[100%]"></div>
       <div class="h-fit w-fit z-30">
         <CardHeader titleKey="user-feedback.card-header.title" subtitleKey="user-feedback.card-header.sub" :iconPath="'speechBubbleImagePath'" class="w-full" icon-class="scale-[1.0] translate-x-[0px] px-[8px] "/>
 
@@ -100,6 +101,7 @@ import speechBubbleImagePath from '../assets/img/text.bubble@8x.png'
 
 /* Import Quote stuff */
 
+import { everyNth } from "~/utils/util";
 import { getUsableQuotes } from '~/utils/quotes';
 const quotes = getUsableQuotes()
 
@@ -111,12 +113,15 @@ const outerContainer:       Ref<HTMLElement|null> = ref(null)
 const innerContent:         Ref<HTMLElement|null> = ref(null)
 const mmfIcon:              Ref<HTMLElement|null> = ref(null)
 const mmfName:              Ref<HTMLElement|null> = ref(null)
-const tagline:              Ref<HTMLElement|null> = ref(null)
+const introTagline:         Ref<HTMLElement|null> = ref(null)
 const downloadButton:       Ref<HTMLElement|null> = ref(null)
 const backgroundContainer:  Ref<HTMLElement|null> = ref(null)
 const colorSplash1:         Ref<HTMLElement|null> = ref(null)
 const colorSplash2:         Ref<HTMLElement|null> = ref(null)
 const taglineContainer:     Ref<HTMLElement|null> = ref(null)
+const tagline:              Ref<HTMLElement|null> = ref(null)
+const quoteContainer:       Ref<HTMLElement|null> = ref(null)
+
 
 /* Constants */
 
@@ -159,7 +164,7 @@ onMounted(() => {
   tlIntro.to(innerContent.value, { translateY: 0, ease: ease}, 0)
   tlIntro.to(mmfIcon.value, { opacity: 1, ease: ease}, 0)
   tlIntro.fromTo(mmfName.value, { opacity: 0.75, scale: 0.8 }, { opacity: 1, scale: 1, ease: ease}, 0)
-  tlIntro.to(tagline.value, { opacity: 1, ease: ease}, 0)
+  tlIntro.to(introTagline.value, { opacity: 1, ease: ease}, 0)
   tlIntro.to(downloadButton.value, { opacity: 1, ease: ease}, 0)
 
   tlIntro.duration(duration)
@@ -167,8 +172,17 @@ onMounted(() => {
 
   /* Setup scroll animation */
 
-  const sections = [4000, 500, 1000, 2000]
-  const sectionsSum = sections.reduce((partialSum, n) => partialSum + n, 0)
+  const containerHeight = outerContainer.value!.offsetHeight
+  const taglineHeight = tagline.value!.offsetHeight
+
+  const zoomDistance = 3000.0
+  const taglineDistance = 1000.0
+  const quotesDistance = quoteContainer.value!.scrollHeight - quoteContainer.value!.offsetHeight
+
+  const taglineShift = -1000.0
+  const quotesShift = 200.0
+
+  const overallDistance = [zoomDistance, taglineDistance, quotesDistance, taglineShift, quotesShift].reduce((partialSum, n) => partialSum + n, 0)
 
   const tlScroll = gsap.timeline({
     scrollTrigger: {
@@ -176,25 +190,41 @@ onMounted(() => {
       pin: true, // Pin the trigger element while active
       anticipatePin: 1, // Prevent jitter when pin becomes active
       start: "top top", // Start when the top of the trigger hits the top of the viewport
-      end: `+=${ sectionsSum }`, // End after scrolling this many px beyond the start
-      scrub: 0.5, // Smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      end: `+=${ overallDistance }`, // End after scrolling this many px beyond the start
+      scrub: 0.0, // Smooth scrubbing, takes x second to "catch up" to the scrollbar
     },
   })
   
   // Add zoom animation to tl
   const scale = 450.0 * window.innerHeight / 970.0
-  tlScroll.to(innerContent.value, { scale: scale, translateY: `${scale * -4.6}rem`, ease: linearScalingEase(scale), duration: sections[0] })
+  tlScroll.to(innerContent.value, { scale: scale, translateY: `${scale * -4.6}rem`, ease: linearScalingEase(scale), duration: zoomDistance })
   // tlScroll.set(innerContent.value, { scale: 1.0 })
   // tlScroll.set(backgroundContainer, { backgroundColor: 'black' })
 
+  // Add tagline fadein animation to tl
+  tlScroll.fromTo(taglineContainer.value, { opacity: 0 }, { opacity: 1, duration: taglineDistance }, `>${ taglineShift }`)
+
+  // Add quotes
+  tlScroll.to({}, { duration: quotesDistance, onUpdate: function() { 
+    
+    const progress = this.progress()
+    const scrollPosition = intervalScale(progress, unitInterval, { start: 0, end: quotesDistance })
+    quoteContainer.value!.scrollTop = scrollPosition
+
+    console.log(`Quotes progress: ${ progress } scrollPos: ${ scrollPosition }`);
+    
+  }}, `>${ quotesShift }`)
+  tlScroll.set(quoteContainer.value!, { visibility: 'visible' }, `<0` )
+  tlScroll.fromTo(quoteContainer.value!, { opacity: 0 }, { opacity: 1, duration: 200 }, `<0`)
+
+  const taglineDistanceToOffscreen = tagline.value!.offsetTop + tagline.value!.offsetHeight
+  const quotesDistanceToTagline = containerHeight/2 - tagline.value!.offsetHeight/2
+
+  tlScroll.fromTo(taglineContainer.value, { opacity: 1, translateY: '0'}, { opacity: 0, translateY: `${ -taglineDistanceToOffscreen }px`, duration: taglineDistanceToOffscreen * 1.3, ease: 'none' }, `<${ quotesDistanceToTagline - 200 }`)
+
+
   // Add pause to tl
-  tlScroll.to({}, { duration: sections[1] }, '+=0')
-
-  // Add tagline animatmion to tl
-  tlScroll.to(taglineContainer.value, { opacity: 1, duration: sections[2] }, '+=0')
-
-    // Add pause to tl
-    tlScroll.to({}, { duration: sections[3] }, '+=0')
+  // tlScroll.to({}, { duration: sections[4] }, '+=0')  
 
 })
 
