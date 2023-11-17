baee756a66eea1a3ab22f74b7abb27409a6ce6ae
Noah Nuebling <noah.n.public@gmail.com>
2023-11-17 06:04:05 +0100

    I like the layout as is. We put huge whitespace between the sections. So it feels like a 'story' or an 'experience'. Which is pretty cool. But all the scrolling might get annoying if you want to actually get information from the page.

--- components/SectionHeader.vue
+++ components/SectionHeader.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="w-full flex flex-col justify-center items-center h-[100svh]">
+  <div class="w-full flex flex-col justify-center items-center">
 
     <!-- Title -->
-    <div class="section-title mx-[1rem] h-full flex items-center">
+    <div class="section-title mx-[1rem] h-full flex items-center mt-[50vh] mb-[50vh]">
       <i18n-t tag="h2" :keypath="titleKey!" class="fadeee text-center whitespace-pre-wrap xs:text-[2.75rem] sm:text-[3.25rem] md:text-[5rem] text-[5.5rem] font-[700] leading-[1.05] tracking-[-0.15rem] text-gray-800">
         <template #accent v-if="titleAccentKey"> 
           <span :class="['', titleAccentClass]">{{ $t(titleAccentKey!)}}</span> 
@@ -14,7 +14,7 @@
     </div>
 
     <!-- Body -->
-    <div class="section-body mx-[1rem] absolute inset-0 h-full border flex justify-center items-center opacity-0">
+    <div class="section-body mx-[1rem] flex justify-center items-center mb-[50vh]">
       <p v-if="bodyKey" v-html="$mt(bodyKey!)" class="fadeee text-center max-w-[30em] whitespace-pre-wrap sm:text-[1.4rem] text-[1.8rem] text-black/[0.9] strong:text-gradient-to-l font-[400] strong:font-[500] leading-[1.4] tracking-[-0.005rem]"></p>
     </div>
   </div>
