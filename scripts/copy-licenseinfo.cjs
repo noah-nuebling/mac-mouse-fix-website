04ca38b763b71eb94bae1ba9686fb2c747ad7c8d
Noah Nuebling <noah.n.public@gmail.com>
2023-11-26 03:55:00 +0100

    Sight documentation improvements

--- scripts/copy-licenseinfo.cjs
+++ scripts/copy-licenseinfo.cjs
@@ -5,7 +5,7 @@
 //    However the contents of the public folder can't be imported inside vue components due to issues with nuxt3. So we also need the licenseinfo folder in the assets folder.
 //  - We call this script manually inside our package.json scripts (the ones called with pnpm xxxx) - before the package.json scripts invoke the nuxt build process. This isn't super pretty, but we tried tons of different things for hours and everything was buggy and terrible. So I'm just glad that this works.
 //  - Alternative solutions:
-//    - 1. Using a post-build script to copy from assets to public -> Files added after build aren't served. See https://github.com/nuxt/nuxt/issues/15779
+//    - 1. Using a post-build script to copy from assets to public -> The files added after build aren't served. See https://github.com/nuxt/nuxt/issues/15779
 //    - 2. Using vite-plugin-static-copy to copy from assets to public -> Doesn't seem to work with SSG. See https://github.com/sapphi-red/vite-plugin-static-copy/discussions/68
 //    - 3. Using nuxt3's useFetch() to access public folder from vue components -> Is buggy. With possible workarounds. See https://github.com/nuxt/nuxt/issues/13857.
 //    - 4. Using custom server routing to make licenseconfig appear at the server root -> This might work well. I didn't test it. See https://github.com/nuxt/nuxt/issues/15779
