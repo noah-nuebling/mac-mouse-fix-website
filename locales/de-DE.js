ce35d2067f4971d346e72b4037f399856fc65044
Noah Nuebling <noah.n.public@gmail.com>
2023-12-01 22:22:46 +0100

    Further simplified the tax disclaimer. Not showing the after tax price estimates anymore. I really don't wanna show the ugly uneven price estimates. I think it's okay.

--- locales/de-DE.js
+++ locales/de-DE.js
@@ -160,7 +160,7 @@ export default {
   'alternatives.title':  "Better value than **Alternatives**",
   'alternatives.body':   "Mac Mouse Fix is **{ priceFactor }x cheaper** compared to alternatives like buying a Logitech MX Master ($99.99 MSRP), or an Apple Trackpad ($129.00 MSRP).\n\nAnd that's even though Mac Mouse Fix - even paired with a cheap mouse - offers a far nicer experience compared a MX Master.", // Keep MXMasterPrice in index.vue in sync with this, so that the price factor is correct.
   
-  'price.disclaimer': "* Der Preis von **${price}** ist ohne die lokale Verbrauchssteuer (wie z.B. der Mehrwertsteuer in Europa).\nDer Steuersatz beträgt ca. **{taxEstimateLow}%** - **{taxEstimateHigh}%**, abhängig von deiner Region, sodass der Endpreis ca. zwischen **${afterTaxPriceEstimateLow}** - **${afterTaxPriceEstimateHigh}** betragen wird.",
+  'price.disclaimer': "Der Preis von **${price}** ist ohne die lokale Verbrauchssteuer (wie z.B. der Mehrwertsteuer in Europa).\nDer Steuersatz beträgt ca. {taxEstimateLow}% - {taxEstimateHigh}%, abhängig von deiner Region.",
 
   
 
