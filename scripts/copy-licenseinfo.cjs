025ebdfb8c21a3b732bcfa2f28421acbd86a1cf6
Noah Nuebling <noah.n.public@gmail.com>
2024-09-20 08:59:54 +0200

    Added Egypt to free countries and added some comments around license config.

--- scripts/copy-licenseinfo.cjs
+++ scripts/copy-licenseinfo.cjs
@@ -1,8 +1,9 @@
 
 // This script copies the licenseinfo folder from the assets folder to the public folder.
 // Discussion:
-//  - The contents of the public folder will be served at the root of the website. Making the licenseConfig accessible at noah-nuebling.github.io/mac-mouse-fix-website/licenseinfo/config.json. This is the URL where the MMF app expects to find the licenseconfig.
-//    However the contents of the public folder can't be imported inside vue components due to issues with nuxt3. So we also need the licenseinfo folder in the assets folder.
+//  - Why do we do this? 
+//      The contents of the public folder will be served at the root of the website. Making the licenseConfig accessible at noah-nuebling.github.io/mac-mouse-fix-website/licenseinfo/config.json. This is the URL where the MMF app expects to find the licenseconfig.
+//      However the contents of the public folder can't be imported inside vue components due to issues with nuxt3. So we also need the licenseinfo folder in the assets folder.
 //  - We call this script manually inside our package.json scripts (the ones called with pnpm xxxx) - before the package.json scripts invoke the nuxt build process. This isn't super pretty, but we tried tons of different things for hours and everything was buggy and terrible. So I'm just glad that this works.
 //  - Alternative solutions:
 //    - 1. Using a post-build script to copy from assets to public -> The files added after build aren't served. See https://github.com/nuxt/nuxt/issues/15779
